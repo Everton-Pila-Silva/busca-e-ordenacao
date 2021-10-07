1811eecadb7e9f755c79e1a0b3e99cccffa2ec33
Everton Pila Silva <84790670+Everton-Pila-Silva@users.noreply.github.com>
2021-10-07 18:20:16 -0300

    Fist commit

---
+++ Modelo/FrmVetor.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Modelo
+{
+    partial class FrmVetor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnBuscaBinaria = new System.Windows.Forms.Button();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.listDados = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listCidades = new System.Windows.Forms.ListBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(0)))), ((int)(((byte)(238)))));
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.btnFechar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(968, 177);
+            this.panel2.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Modelo.Properties.Resources.close;
+            this.button1.Location = new System.Drawing.Point(850, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 80);
+            this.button1.TabIndex = 18;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Modelo.Properties.Resources.sinfo;
+            this.pictureBox1.Location = new System.Drawing.Point(217, -5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(513, 209);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Image = global::Modelo.Properties.Resources.close;
+            this.btnFechar.Location = new System.Drawing.Point(936, 0);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(148, 125);
+            this.btnFechar.TabIndex = 12;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 37);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Busca e Ordenação em C#";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(26, 240);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(917, 26);
+            this.txtEntrada.TabIndex = 0;
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrdenar.Location = new System.Drawing.Point(180, 294);
+            this.btnOrdenar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(90, 44);
+            this.btnOrdenar.TabIndex = 18;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.BtnOrdenar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Location = new System.Drawing.Point(310, 294);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(157, 44);
+            this.btnBuscar.TabIndex = 19;
+            this.btnBuscar.Text = "Busca Sequencial";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // btnBuscaBinaria
+            // 
+            this.btnBuscaBinaria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscaBinaria.Location = new System.Drawing.Point(492, 294);
+            this.btnBuscaBinaria.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBuscaBinaria.Name = "btnBuscaBinaria";
+            this.btnBuscaBinaria.Size = new System.Drawing.Size(129, 44);
+            this.btnBuscaBinaria.TabIndex = 20;
+            this.btnBuscaBinaria.Text = "Busca Binaria";
+            this.btnBuscaBinaria.UseVisualStyleBackColor = true;
+            this.btnBuscaBinaria.Click += new System.EventHandler(this.BtnBuscaBinaria_Click);
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCarregar.Location = new System.Drawing.Point(671, 294);
+            this.btnCarregar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(105, 44);
+            this.btnCarregar.TabIndex = 21;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.BtnCarregar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Location = new System.Drawing.Point(819, 294);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(105, 44);
+            this.btnSalvar.TabIndex = 22;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // listDados
+            // 
+            this.listDados.FormattingEnabled = true;
+            this.listDados.ItemHeight = 19;
+            this.listDados.Location = new System.Drawing.Point(58, 398);
+            this.listDados.Name = "listDados";
+            this.listDados.Size = new System.Drawing.Size(409, 137);
+            this.listDados.TabIndex = 23;
+            this.listDados.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            this.listDados.SelectedValueChanged += new System.EventHandler(this.listCidades_SelectedValueChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Location = new System.Drawing.Point(58, 294);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 44);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // listCidades
+            // 
+            this.listCidades.FormattingEnabled = true;
+            this.listCidades.ItemHeight = 19;
+            this.listCidades.Location = new System.Drawing.Point(492, 398);
+            this.listCidades.Name = "listCidades";
+            this.listCidades.Size = new System.Drawing.Size(409, 137);
+            this.listCidades.TabIndex = 24;
+            // 
+            // FrmVetor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(968, 586);
+            this.Controls.Add(this.listCidades);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listDados);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.btnBuscaBinaria);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmVetor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FrmVetor_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnBuscaBinaria;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.ListBox listDados;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listCidades;
+    }
+}
---
+++ Modelo/FrmVetor.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelo
+{
+    public partial class FrmVetor : Form
+    {
+        List<String> nomes = new List<String>();
+        List<String> cidade = new List<string>(new string[100]);
+        public FrmVetor()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmVetor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnOrdenar_Click(object sender, EventArgs e)
+        {
+            nomes.Sort();
+            cidade.Sort();
+            mostra();
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            int p = nomes.IndexOf(txtEntrada.Text);
+            if (p == -1)
+                MessageBox.Show("Não encontrado na lista de nomes :(");
+            else
+                MessageBox.Show("Encontrado no 1° quadrante na posicao:" + p);
+
+            int j = cidade.IndexOf(txtEntrada.Text);
+            if (j == -1)
+                MessageBox.Show("Não encontrado na lista de cidades :(");
+            else
+                MessageBox.Show("Encontrado no 2° quadrante na posicao:" + j);
+        }
+
+        private void BtnBuscaBinaria_Click(object sender, EventArgs e)
+        {
+            int p = nomes.BinarySearch(txtEntrada.Text);
+
+            if (p < 0)
+                MessageBox.Show("Não encontrado na lista de nomes :(" + p);
+            else
+                MessageBox.Show("Nome Encontrado na posicao:" + p);
+
+            int j = cidade.BinarySearch(txtEntrada.Text);
+
+            if (j < 0)
+                MessageBox.Show("Não encontrado na lista de cidadedes :(" + j);
+            else
+                MessageBox.Show("Cidade encontrada na posicao:" + j);
+
+        }
+
+        private void BtnCarregar_Click(object sender, EventArgs e)
+        {
+            StreamReader arquivo = new StreamReader("DUPLA_(9).txt");
+            string linha;
+            int c = 0;
+            var n = 0;
+        
+            using (var reader = File.OpenText("DUPLA_(9).txt"))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    n++;
+                }
+            }
+            
+            do
+            {
+                linha = arquivo.ReadLine();
+                listCidades.Items.Add(linha); // listCidades é um ListBox
+                cidade[c] = linha; // cidades é um vetor, declarado como variável global da classe          
+                c++;
+            } while (c < n); // n é a quantidade de elementos do arquivo
+            arquivo.Close();
+            MessageBox.Show("Dados Carregados");
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            StreamWriter arquivo = new StreamWriter("ordenado.txt");
+            foreach (string c in cidade)
+                arquivo.WriteLine(c);
+            MessageBox.Show("Dados Salvos!");
+            arquivo.Close();
+        }
+
+        private void listCidades_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        void mostra()
+        {
+            listDados.Items.Clear();
+            listCidades.Items.Clear();
+
+            foreach (String n in nomes)
+                listDados.Items.Add(n);
+
+            foreach (String n in cidade)
+                listCidades.Items.Add(n);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            nomes.Add(txtEntrada.Text);
+            mostra();
+            txtEntrada.Clear();
+            txtEntrada.Focus();
+        }
+    }
+}
